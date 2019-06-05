099e391fbe7f0987e1d05facd9b053f5dc9aace6
dmitry <mordasdsp@gmail.com>
2019-06-05 06:13:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    var config = {
+        type: graphics_mode,
+        width: game_width,
+        height: game_height,
+        parent: 'div-tag-name',
+        scene: [nameOfScene]
+    };
+}
